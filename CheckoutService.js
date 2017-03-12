c514f22ed8aabf3ec7e9b884abd85ae6439917d6
GiovanyReyes <giovanyRReyes@gmail.com>
2017-03-11 17:31:34 -0800

    Angular Services

---
+++ CheckoutService.js
@@ -0,0 +1,28 @@
+angular.module('public.checkout')
+    .factory('CheckoutServices', CheckoutServicesFactory)
+CheckoutServicesFactory.$inject = ['$http']
+
+function CheckoutServicesFactory($http) {
+    return {
+        getSavedAddress,
+        submitOrder
+    }
+
+    function getSavedAddress(id, onSuccess, onError) {
+        $http.get('/api/addresses' + id)
+            .then(function(response) {
+                onSuccess(response.data)
+            }).catch(function(response) {
+                onError(response.data)
+            })
+    }
+
+    function submitOrder(data, onSuccess, onError) {
+        $http.post('/api/orders/checkout', data)
+            .then(function(response) {
+                onSuccess(response.data)
+            }).catch(function(response) {
+                onError(response.data)
+            })
+    }
+}
